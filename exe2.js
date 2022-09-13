d0b94b0d7423a9bab0e53d5c99e48b004f3ca2a7
israell <israelpina047@gmail.com>
2022-09-13 02:19:15 -0300

    Mudanças

---
+++ exe2.js
@@ -0,0 +1,22 @@
+let palavras = [
+    "a",
+    "b",
+    "c"
+
+]
+
+let n = [
+    1,
+    2,
+    3
+]
+
+function uniao(arrei) {
+    let novoArray = [];
+    for (const b of arrei) {
+        novoArray.push(b)
+
+    }
+    console.log(novoArray.concat())
+}
+uni(palavras,n)
